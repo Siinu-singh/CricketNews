2afbbb6ea4a5c1b51d069ab01441cc5062f6951a
Suraj singh <surajgpes@gmail.com>
2025-05-26 19:05:39 +0000

    make the hero section touch at the top and navbar should come on the top of hero section.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -67,7 +67,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
         <Header />
-        <main className="flex-grow pt-16"> {/* Changed: Removed container, mx-auto, px-8 */}
+        <main className="flex-grow"> {/* Removed pt-16 */}
           {children}
         </main>
         <Footer />
